5a645e56ba926a4c291347a806dd9e80f4c5e448
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-10-16 18:45:30 +0300

    Temporarily disabled Messenger Chat

--- layouts/FbMessenger.jsx
+++ layouts/FbMessenger.jsx
@@ -1,14 +1,12 @@
-import MessengerCustomerChat from "react-messenger-customer-chat";
-
 const FbMessenger = ({ children }) => (
 	<>
 		{children}
 
-		<MessengerCustomerChat
+		{/* <MessengerCustomerChat
 			pageId='166364938207886'
 			appId='1128654474421430'
 			className='fb-messenger-chat-comp'
-		/>
+		/> */}
 	</>
 );
 
